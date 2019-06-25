4cd5b373a10aadc209fc7a9598834ceed2ad2c6b
Михаил Минеев <C:\Users\mixno\AppData\Roaming\The Bat!>
2019-06-25 17:44:37 +0800

    Added PyPi

---
+++ sdc-api/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+SDC API Wrapper
+~~~~~~~~~~~~~~~~~~~
+A basic async wrapper for the SDC API.
+"""
+
+__title__ = 'sdc-api'
+__author__ = 'Tomori Project'
+__version__ = '0.1.0'
+
+VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
+
+version_info = VersionInfo(major=0, minor=1, micro=0, releaselevel='beta', serial=0)
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sdc-api",
+    version="0.1.0",
+    author="Tomori Project",
+    author_email="dev@discord.band",
+    description="An async wrapper for Server-Discord.Com API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TomoriBot/sdc-api",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
