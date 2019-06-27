efa3a2fcf5d46cb79c7290d620aa651c6a996a83
Михаил Минеев <C:\Users\mixno\AppData\Roaming\The Bat!>
2019-06-27 23:45:40 +0800

    Update __init__.py

--- sdc_api/__init__.py
+++ sdc_api/__init__.py
@@ -6,10 +6,10 @@ SDC API Wrapper
 A basic async wrapper for the SDC API.
 """
 
-__title__ = 'sdc-api'
+__title__ = 'sdc_api'
 __author__ = 'Tomori Project'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
 
-version_info = VersionInfo(major=0, minor=2, micro=0, releaselevel='beta', serial=0)
+version_info = VersionInfo(major=0, minor=2, micro=1, releaselevel='beta', serial=0)
