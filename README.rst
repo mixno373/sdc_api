4feb72e385798ad8391169ade76933618e87f76a
Михаил Минеев <C:\Users\mixno\AppData\Roaming\The Bat!>
2019-06-27 19:28:33 +0800

    Updated to 0.2.0
    Added:
    - Votes (guild and user)
    
    Changes:
    - some fixes

--- README.rst
+++ README.rst
@@ -23,9 +23,44 @@ Quick Example
 	# Get guild's place
 	place = await guild.place()
 	print("Place of server in SDC is %d" % place)
-    
+
 Get Guild
 
 .. code:: py
 
 	guild = await sdc.get_guild('id')
+
+Get Guild Votes
+
+.. code:: py
+
+	guild = Guild(id='id')
+	votes = await guild.get_votes()
+
+.. code:: py
+
+	votes = await sdc.get_guild_votes('id')
+
+Get Guild Place
+
+.. code:: py
+
+	place = await sdc.get_guild_place('id')
+	print("Place of server in SDC is %d" % place)
+
+.. code:: py
+
+	guild = await sdc.get_guild('id')
+	place = await guild.place()
+	print("Place of server in SDC is %d" % place)
+
+Get User's Votes
+
+.. code:: py
+
+	user = User(id='id')
+	votes = await user.get_votes()
+
+.. code:: py
+
+	votes = await sdc.get_user_votes('id')
--- sdc-api/sdc-api.py
+++ sdc-api/sdc-api.py
@@ -3,6 +3,48 @@ from aiohttp import ClientSession
 from datetime import datetime, timedelta
 
 
+
+class Vote:
+    def __init__(self, *args, **kwargs):
+        self.guild = kwargs.get("guild", Guild())
+        self.user = kwargs.get("user", User(id=kwargs.get("id")))
+        self.count = kwargs.get("count", 0)
+
+        if not isinstance(self.guild, Guild):
+            self.guild = Guild(id=kwargs.get("guild"))
+        if not isinstance(self.user, User):
+            self.user = User(id=kwargs.get("user"))
+
+        self._api = kwargs.get("api", SDC())
+
+
+    def __str__(self):
+        return "<Vote | Guild: {0.guild.name}, User: {0.user.name}#{0.user.discriminator}, ID: {0.user.id}, count: {0.count}>".format(self)
+
+    def __repr__(self):
+        return "<Vote | Guild: {0.guild.name}, User: {0.user.name}#{0.user.discriminator}, ID: {0.user.id}, count: {0.count}>".format(self)
+
+
+class User:
+    def __init__(self, *args, **kwargs):
+        self.name = kwargs.get("name", "Deleted User")
+        self.discriminator = kwargs.get("discriminator", "0000")
+        self.id = kwargs.get("id", "")
+        self.votes = []
+        self.total_votes = 0
+        self._api = kwargs.get("api", SDC())
+
+    async def get_votes(self):
+        return await self._api.get_user_votes(user=self)
+
+
+    def __str__(self):
+        return "<User | Name: {0.name}#{0.discriminator}, ID: {0.id}>".format(self)
+
+    def __repr__(self):
+        return "<User | Name: {0.name}#{0.discriminator}, ID: {0.id}>".format(self)
+
+
 class Guild:
     def __init__(self, *args, **kwargs):
         self.name = kwargs.get("name", "New Guild")
@@ -12,17 +54,23 @@ class Guild:
         self.online = kwargs.get("online", 0)
         self.members = kwargs.get("members", 0)
         self.ups = kwargs.get("upCount", 0)
+        self.votes = []
+        self.total_votes = 0
         self._api = kwargs.get("api", SDC())
 
-    async def place(self):
-        return await self._api.get_place(self.id)
+
+    async def get_place(self):
+        return await self._api.get_guild_place(self.id, guild=self)
+
+    async def get_votes(self):
+        return await self._api.get_guild_votes(guild=self)
 
 
     def __str__(self):
-        return "<Guild: {0.name}, ID: {0.id}, Description: {0.description}, Invite: {0.invite}, Members: {0.online}/{0.members}, UPs: {0.ups}>".format(self)
+        return "<Guild | Name: {0.name}, ID: {0.id}, Description: {0.description}, Invite: {0.invite}, Members: {0.online}/{0.members}, UPs: {0.ups}, Votes: {0.votes}>".format(self)
 
     def __repr__(self):
-        return "<Guild: {0.name}, ID: {0.id}, Description: {0.description}, Invite: {0.invite}, Members: {0.online}/{0.members}, UPs: {0.ups}>".format(self)
+        return "<Guild | Name: {0.name}, ID: {0.id}, Description: {0.description}, Invite: {0.invite}, Members: {0.online}/{0.members}, UPs: {0.ups}, Votes: {0.votes}>".format(self)
 
 
 class SDC:
@@ -30,6 +78,7 @@ class SDC:
         self.token = kwargs.get("token")
         self.last_request = datetime.utcnow()-timedelta(seconds=3)
         self.route = "https://api.server-discord.com"
+
 
     def req_cd(func):
         async def check_time(*args, **kwargs):
@@ -74,11 +123,12 @@ class SDC:
                             type=error.get("type", "Unknown"),
                             text=error.get("message", "Unknown")
                         ))
+                        return {}
                     return resp
                 elif response.status == 401:
                     raise Exception("Unauthorized")
                 else:
-                    return None
+                    return {}
 
     async def get_guild(self, id=None):
         resp = await self._request("get", "guild", id=id)
@@ -86,6 +136,40 @@ class SDC:
         resp["api"] = self
         return Guild(**resp)
 
-    async def get_place(self, id=None):
+    async def get_guild_place(self, *args, **kwargs):
+        id = kwargs.get("id")
+        if not id:
+            id = args[0] if len(args) > 0 else None
         resp = await self._request("get", "guild", id=id, special="place")
         return resp.get("place", -1)
+
+    async def get_guild_votes(self, *args, **kwargs):
+        guild = kwargs.get("guild")
+        if not isinstance(guild, Guild):
+            _id = args[0] if len(args) > 0 else None
+            guild = await self.get_guild(kwargs.get("id", _id))
+        id = kwargs.get("id", guild.id)
+        guild.votes = []
+        guild.total_votes = 0
+        resp = await self._request("get", "guild", id=id, special="rated")
+        if resp:
+            for id, count in resp.items():
+                vote = Vote(guild=guild, id=id, count=count)
+                guild.votes.append(vote)
+                guild.total_votes += vote.count
+        return guild.votes
+
+    async def get_user_votes(self, *args, **kwargs):
+        user = kwargs.get("user")
+        if not isinstance(user, User):
+            _id = args[0] if len(args) > 0 else None
+            user = User(id=kwargs.get("id", _id))
+        user.votes = []
+        user.total_votes = 0
+        resp = await self._request("get", "user", id=user.id, special="rated")
+        if resp:
+            for id, count in resp.items():
+                vote = Vote(guild=id, user=user, count=count)
+                user.votes.append(vote)
+                user.total_votes += vote.count
+        return user.votes
