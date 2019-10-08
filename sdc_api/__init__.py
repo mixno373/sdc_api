7c447090256e0130929e135b63d53c10e7931e95
Michael <26341323+TomoriBot@users.noreply.github.com>
2019-10-08 20:14:41 +0700

    Update __init__.py

--- sdc_api/__init__.py
+++ sdc_api/__init__.py
@@ -8,8 +8,8 @@ A basic async wrapper for the SDC API.
 
 __title__ = 'sdc_api'
 __author__ = 'Tomori Project'
-__version__ = '0.2.1'
+__version__ = '1.0.0'
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
 
-version_info = VersionInfo(major=0, minor=2, micro=1, releaselevel='beta', serial=0)
+version_info = VersionInfo(major=1, minor=0, micro=0, releaselevel='beta', serial=0)
