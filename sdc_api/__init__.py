abfcadbc4496f147a8755f86e10434e0eb842ba8
Michael <26341323+TomoriBot@users.noreply.github.com>
2019-10-08 20:40:02 +0700

    Update __init__.py

--- sdc_api/__init__.py
+++ sdc_api/__init__.py
@@ -10,6 +10,4 @@ __title__ = 'sdc_api'
 __author__ = 'Tomori Project'
 __version__ = '1.0.0'
 
-VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
-
-version_info = VersionInfo(major=1, minor=0, micro=0, releaselevel='beta', serial=0)
+VERSION = '1.0.0'
